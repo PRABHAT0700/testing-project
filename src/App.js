e72a9184281a9ec73fc9be5a3d0e20f4800d2bda
Prabhat mishra <iammishraprabhat@gmail.com>
2023-09-30 22:39:10 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Covid from "./component/covid"
+
+const App = () => {
+  return (
+    <>
+    
+     <h2>COVID-19 CORONAVIRUS TRACKER</h2>
+     
+    </>
+  )
+}
+
+export default App
---
+++ src/component/covid.js
@@ -0,0 +1,28 @@
+import React, { useEffect } from "react";
+
+const Covid = () => {
+
+    const getCovidData = async () => {
+
+        try{
+            const res = await fetch('https://api.covid19india.org/data.json')
+            console.log(res);
+        }
+        catch(err){
+            console.log(err);
+        }
+        
+
+    }
+
+    useEffect(() => {
+        getCovidData();
+    }, []);
+
+    return (
+        <>
+         <h1>Live</h1>
+         <h2>COVID 19 CORONAVIRUS TRACKER</h2>
+        </>
+    )
+}
